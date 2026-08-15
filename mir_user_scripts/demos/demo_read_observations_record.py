48c96df3f7a132601809fdcf565a70feac53f25d
MichelBtn <michel.btn@live.fr>
2026-08-15 15:55:05 +0200

    feat: add demo script to read and visualize recorded observations from npz files

---
+++ mir_user_scripts/demos/demo_read_observations_record.py
@@ -0,0 +1,27 @@
+import numpy as np
+from pathlib import Path
+from mir_utils.metrics import DataRecorder
+import matplotlib.pyplot as plt
+
+path = Path(__file__).parent.parent.parent / "data/rec.npz"
+data, metadata = DataRecorder.load(path)    
+
+fig, axs = plt.subplots(len(metadata), 1, figsize=(6, len(metadata)*2))
+idx = 0
+for name, entry in metadata.items():
+    array = data[name]
+    if isinstance(array, np.ndarray):
+        min = np.min(array)
+        max = np.max(array)
+        mean = np.mean(array)
+        std = np.std(array)
+        info=(f"Min={min:.1f} Max={max:.1f} Mean={mean:.1f} Std={std:.1f}")
+        axs[idx].plot(array)
+        axs[idx].set_title(f"{name} : {info}", fontsize=9)  # Ajoute un titre à la figure
+        axs[idx].set_xlabel('Index',fontsize=9)
+        axs[idx].set_ylabel(entry['unit'],fontsize=9)
+        axs[idx].tick_params(axis='both', labelsize=8) 
+        axs[idx].grid(True)
+        idx += 1
+plt.tight_layout()         
+plt.show()  
